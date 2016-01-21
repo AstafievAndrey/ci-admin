b35e4c391a3c892e17019a8ab19896d4f9da3f9a
andrejastafev <carrats@yandex.ru>
2016-01-21 12:06:30 +0300

    Убрал лишнее

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,15 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'main';
-$route['address'] = 'main/address';
-$route['otzyvy'] = 'main/otzyvy';
-$route['statya/(:any)'] = 'main/statya/$1';
-$route['stati'] = 'main/stati';
-$route['stati/(:num)'] = 'main/stati/$1';
-$route['divan/(:any)'] = 'main/divan/$1';
-$route['katalog'] = 'main/katalog';
-$route['katalog/(:any)'] = 'main/katalog/$1';
-$route['katalog/(:any)/(:num)'] = 'main/katalog/$1/$2';
 
 //роутинг для админки
 $route['admin'] = 'admin/index';
